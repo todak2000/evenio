a2ef9b3f5107d5ae08287ca00f93a1a3d07b58a2
Daniel Olagunju <todak2000@gmail.com>
2022-09-30 20:00:46 +0100

    isaca

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,4 @@
-import logo from '../../assets/logo.png';
+import logo from '../../assets/logo.svg';
 import './footer.css';
 
 function Footer() {
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import logo from '../../assets/logo.png';
+import logo from '../../assets/logo.svg';
 import hamburger from '../../assets/hamburger.png';
 import close from '../../assets/close.png';
 import { useState } from 'react';
