2e80e2403c93b751e2e1fc3726ce7e52681fa360
Daniel Olagunju <todak2000@gmail.com>
2022-09-30 22:03:53 +0100

    reverting done

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import logo from '../../assets/logo.svg';
+import logo from '../../assets/logo.png';
 import hamburger from '../../assets/hamburger.png';
 import close from '../../assets/close.png';
 import { useState } from 'react';
