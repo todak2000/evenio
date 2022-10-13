a54f8aa2e0d5a246f331becdd2232d27164b2b72
Daniel Olagunju <todak2000@gmail.com>
2022-10-13 22:07:02 +0100

    consuming schedule api locally

--- src/components/Schedule/data.js
+++ src/components/Schedule/data.js
@@ -4,7 +4,43 @@ import personThree from '../../assets/personThree.jpeg';
 import personFour from '../../assets/personFour.jpeg';
 // import lunch from '../../assets/lunch.jpeg';
 
-export const dayOne = [
+export const months = [
+  {
+    id: 1,
+    month: "October",
+  },
+  {
+    id: 2,
+    month: "November",
+  },
+  {
+    id: 3,
+    month: "December",
+  }
+]
+
+
+export const imageDummy = [
+  {
+    imageOne: personOne,
+    imageTwo: personTwo,
+  },
+  {
+    imageOne: personThree,
+    imageTwo: personTwo,
+  },
+  {
+    imageOne: personThree,
+    imageTwo: personFour,
+  },
+  {
+    imageOne: personTwo,
+    imageTwo: personFour,
+  },
+]
+
+
+export const dayOneDummy = [
     {
       id: 1,
       title: "Introduction Business",
@@ -17,7 +53,7 @@ export const dayOne = [
       // room: "Room #2"
     },
   ]
-  export const dayThree = [
+  export const dayThreeDummy = [
     {
       id: 1,
       title: "Cultures of Creativity",
@@ -30,7 +66,7 @@ export const dayOne = [
     
   ]
 
-  export const dayTwo = [
+  export const dayTwoDummy = [
     {
       id: 1,
       title: "Marketing Strategy",
