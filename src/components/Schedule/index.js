5d8fd206ef871c74374bd084cc74c60b1b435293
Daniel Olagunju <todak2000@gmail.com>
2022-10-28 11:33:58 +0100

    fixing bug

--- src/components/Schedule/index.js
+++ src/components/Schedule/index.js
@@ -35,30 +35,30 @@ const getEventSchedule = async () => {
   try {
       const resp = await axios.get(`${url}/isaca-events`);
 
-      let months = resp.data.months
+      let months = resp?.data?.months
       
       setMonthsData(months)
       console.log(resp.data.events[0].month, "efefe")
       // month one
-      let one = resp.data.events.filter((item)=>item.month === monthsData[0].month).map(filteredItemDayOne=>{
+      let one = resp?.data?.events.filter((item)=>item?.month === monthsData[0]?.month).map(filteredItemDayOne=>{
         return filteredItemDayOne
       })
       setDayOne(one)
       setData(one)
       // console.log(one, "one")
       // month two
-      let two = resp.data.events.filter((item)=>item.month === monthsData[1].month).map(filteredItemDayTwo=>{
+      let two = resp?.data?.events.filter((item)=>item?.month === monthsData[1]?.month).map(filteredItemDayTwo=>{
         return filteredItemDayTwo
         
       })
       setDayTwo(two)
       // month three
-      let three = resp.data.events.filter((item)=>item.month === monthsData[2].month).map(filteredItemDayThree=>{
+      let three = resp?.data?.events.filter((item)=>item?.month === monthsData[2].month).map(filteredItemDayThree=>{
         return filteredItemDayThree
         
       })
       setDayThree(three)
-      console.log(resp.data.events)
+      console.log(resp?.data?.events)
       // return resp.data.events
   } catch (err) {
       // Handle Error Here
@@ -87,8 +87,8 @@ useEffect(() => {
         <section className='tab'>
         {monthsData?.map((item)=>{
           return(
-            <div key={item.id} className={select === item.id ? 'tab-card-active': 'tab-card'} onClick={()=>{handleSelect(item.id)}}>
-            <h2>{item.month}</h2>
+            <div key={item?.id} className={select === item?.id ? 'tab-card-active': 'tab-card'} onClick={()=>{handleSelect(item.id)}}>
+            <h2>{item?.month}</h2>
           </div>
           )
         })}
